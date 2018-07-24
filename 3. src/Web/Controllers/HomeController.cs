d4a0bc0066a0d20bf0a2f2a6a48ab9ade7cf137a
Lê Văn Hiếu <hieu.le@restaff.no>
2018-07-24 17:18:10 +0700

    add Search View

--- 3. src/Web/Controllers/HomeController.cs
+++ 3. src/Web/Controllers/HomeController.cs
@@ -16,108 +16,188 @@ using Services.UserSerivce;
 using Web.ViewModels.Home;
 namespace Web.Controllers
 {
-	[Authorize]
-	public class HomeController : Controller
-	{
+  [Authorize]
+  public class HomeController : Controller
+  {
 
-		private IDokmeeService _dokmeeService;
-		private IMapper _mapper;
-		private ISessionHelperService _sessionHelperService;
-	    private IUserService _userService;
+    private IDokmeeService _dokmeeService;
+    private IMapper _mapper;
+    private ISessionHelperService _sessionHelperService;
+    private IUserService _userService;
 
-		public HomeController(IDokmeeService dokmeeService, IMapper mapper, ISessionHelperService sessionHelperService, IUserService userService)
-		{
-			_dokmeeService = dokmeeService;
-			_mapper = mapper;
-			_sessionHelperService = sessionHelperService;
-		    _userService = userService;
-		}
+    public HomeController(IDokmeeService dokmeeService, IMapper mapper, ISessionHelperService sessionHelperService, IUserService userService)
+    {
+      _dokmeeService = dokmeeService;
+      _mapper = mapper;
+      _sessionHelperService = sessionHelperService;
+      _userService = userService;
+    }
 
-		public ActionResult Index()
-		{
-			try
-			{
-				IndexModel model = new IndexModel();
-				IEnumerable<DokmeeCabinet> dokmeeCabinets = _dokmeeService.GetCurrentUserCabinet(User.Identity.GetUserId());
-				model.Cabinets = _mapper.Map<IEnumerable<Cabinet>>(dokmeeCabinets);
-				return View(model);
-			}
-			catch (InvalideUsernameException ex)
-			{
-				return RedirectToAction("Logoff", "Account");
-			}
-			catch (InvalidePasswordException ex)
-			{
-				return RedirectToAction("Logoff", "Account");
-			}
-			catch (Exception ex)
-			{
-				throw;
-			}
-		}
+    public ActionResult Index()
+    {
+      try
+      {
+        IndexModel model = new IndexModel();
+        IEnumerable<DokmeeCabinet> dokmeeCabinets = _dokmeeService.GetCurrentUserCabinet(User.Identity.GetUserId());
+        model.Cabinets = _mapper.Map<IEnumerable<Cabinet>>(dokmeeCabinets);
+        return View(model);
+      }
+      catch (InvalideUsernameException ex)
+      {
+        return RedirectToAction("Logoff", "Account");
+      }
+      catch (InvalidePasswordException ex)
+      {
+        return RedirectToAction("Logoff", "Account");
+      }
+      catch (Exception ex)
+      {
+        throw;
+      }
+    }
 
-		public ActionResult AfterMyActionResult(string username, string password, ConnectorType loginType)
-		{
-			Session["abc"] = "123";
-			_sessionHelperService.Username = username;
-			_sessionHelperService.Password = password;
-			_sessionHelperService.ConnectorType = loginType;
-			return RedirectToAction("Index");
-		}
+    public ActionResult AfterMyActionResult(string username, string password, ConnectorType loginType)
+    {
+      Session["abc"] = "123";
+      _sessionHelperService.Username = username;
+      _sessionHelperService.Password = password;
+      _sessionHelperService.ConnectorType = loginType;
+      return RedirectToAction("Index");
+    }
 
-		public ActionResult CabinetDetail(string cabinetId)
-		{
-			string username = User.Identity.GetUserId();
-			IEnumerable<DmsNode> cabinetContent = _dokmeeService.GetCabinetContent(cabinetId, username);
-			IEnumerable<Node> nodes = _mapper.Map<IEnumerable<Node>>(cabinetContent);
-			ViewBag.cabinetId = cabinetId;
-			return View(nodes);
-		}
+    public ActionResult CabinetDetail(string cabinetId)
+    {
+      string username = User.Identity.GetUserId();
+      IEnumerable<DmsNode> cabinetContent = _dokmeeService.GetCabinetContent(cabinetId, username);
+      IEnumerable<Node> nodes = _mapper.Map<IEnumerable<Node>>(cabinetContent);
+      ViewBag.cabinetId = cabinetId;
+      return View(nodes);
+    }
 
-		public ActionResult Details(string cabinetId, string dmstype, string name)
-		{
-			if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(dmstype)
-					   && !string.IsNullOrEmpty(cabinetId))
-			{
-				var isFolder = dmstype.ToUpper().Trim() == "FOLDER" ? true : false;
-				string username = User.Identity.GetUserId();
-				DetailModel model = new DetailModel();
-				IEnumerable<DokmeeFilesystem> dokmeeFilesystems = _dokmeeService.GetDokmeeFilesystems(username, name, isFolder, cabinetId);
-				model.dokmeeFilesystems = dokmeeFilesystems;
-				ViewBag.cabinetId = cabinetId;
-				return View(model);
-			}
-			else return View();
-		}
+    public ActionResult Details(string cabinetId, string dmstype, string name)
+    {
+      if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(dmstype)
+             && !string.IsNullOrEmpty(cabinetId))
+      {
+        var isFolder = dmstype.ToUpper().Trim() == "FOLDER" ? true : false;
+        string username = User.Identity.GetUserId();
+        DetailModel model = new DetailModel();
+        IEnumerable<DokmeeFilesystem> dokmeeFilesystems = _dokmeeService.GetDokmeeFilesystems(username, name, isFolder, cabinetId);
+        model.dokmeeFilesystems = dokmeeFilesystems;
+        ViewBag.cabinetId = cabinetId;
+        return View(model);
+      }
+      else return View();
+    }
 
-		public async Task<ActionResult> Folder(string id, bool isRoot)
-		{
-			string username = User.Identity.GetUserId();
-			var contents = await _dokmeeService.GetFolderContent(username, id, isRoot);
-			IEnumerable<Node> nodes = _mapper.Map<IEnumerable<Node>>(contents);
-			return View(nodes);
-		}
+    public async Task<ActionResult> Folder(string id, bool isRoot)
+    {
+      string username = User.Identity.GetUserId();
+      var contents = await _dokmeeService.GetFolderContent(username, id, isRoot);
+      IEnumerable<Node> nodes = _mapper.Map<IEnumerable<Node>>(contents);
+      return View(nodes);
+    }
 
-		[HttpPost]
-		public ActionResult UpdateStatus(Dictionary<object, object> args)
-		{
-			if (args != null && args.ContainsKey("CustomerStatus"))
-			{
-				string username = User.Identity.GetUserId();
-				_dokmeeService.UpdateIndex(username, args);
-			}
-			return Json(new { });
-		}
+    [HttpPost]
+    public ActionResult UpdateStatus(Dictionary<object, object> args)
+    {
+      if (args != null && args.ContainsKey("CustomerStatus"))
+      {
+        string username = User.Identity.GetUserId();
+        _dokmeeService.UpdateIndex(username, args);
+      }
+      return Json(new { });
+    }
 
-		//[HttpPost]
-		public ActionResult Preview(string id, string cabinetId)
-		{
-			if (!string.IsNullOrEmpty(id)&& !string.IsNullOrEmpty(cabinetId))
-			{
-				string username = User.Identity.GetUserId();
-				_dokmeeService.Preview(username, id, cabinetId);
-			}
-			return Json(new { });
-		}
-	}
+    //[HttpPost]
+    public ActionResult Preview(string id, string cabinetId)
+    {
+      if (!string.IsNullOrEmpty(id) && !string.IsNullOrEmpty(cabinetId))
+      {
+        string username = User.Identity.GetUserId();
+        _dokmeeService.Preview(username, id, cabinetId);
+      }
+      return Json(new { });
+    }
+
+    public ActionResult Search(string key)
+    {
+      SearchModel model = new SearchModel();
+      model.Key = key;
+
+      model.TableTitles.Add(new DocumentIndex()
+      {
+        Order = 1,
+        Title = "Customer ID"
+      });
+      model.TableTitles.Add(new DocumentIndex()
+      {
+        Order = 2,
+        Title = "Customer Name"
+      });
+      model.TableTitles.Add(new DocumentIndex()
+      {
+        Order = 3,
+        Title = "SIGN UP"
+      });
+
+
+      model.DocumentItems.Add(new DocumentItem()
+      {
+        Id = "aa1",
+        Title = "Forder 1",
+        Indexs = new List<DocumentIndex>(),
+        IsFolder = true,
+        IsRoot = true,
+        Type = "folder"
+      });
+      model.DocumentItems.Add(new DocumentItem()
+      {
+        Id = "aa2",
+        Title = "Forder 2",
+        Indexs = new List<DocumentIndex>(),
+        IsFolder = true,
+        IsRoot = true,
+        Type = "folder"
+      });
+      model.DocumentItems.Add(new DocumentItem()
+      {
+        Id = "aa3",
+        Title = "Forder 3",
+        Indexs = new List<DocumentIndex>(),
+        IsFolder = true,
+        IsRoot = true,
+        Type = "folder"
+      });
+
+      model.DocumentItems.Add(new DocumentItem()
+      {
+        Id = "bb1",
+        Title = "file 1",
+        Indexs = new List<DocumentIndex>(),
+        IsFolder = false,
+        IsRoot = false,
+        Type = "pdf"
+      });
+      model.DocumentItems.Add(new DocumentItem()
+      {
+        Id = "bb1",
+        Title = "file 2",
+        Indexs = new List<DocumentIndex>(),
+        IsFolder = false,
+        IsRoot = false,
+        Type = "pdf"
+      });
+      model.DocumentItems.Add(new DocumentItem()
+      {
+        Id = "bb3",
+        Title = "file 3",
+        Indexs = new List<DocumentIndex>(),
+        IsFolder = false,
+        IsRoot = false,
+        Type = "png"
+      });
+      return View(model);
+    }
+  }
 }
---
+++ 3. src/Web/ViewModels/Home/SearchModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.ViewModels.Home
+{
+  public class SearchModel
+  {
+    public string Key { get; set; }
+    public List<DocumentIndex> TableTitles { get; set; } = new List<DocumentIndex>();
+
+    public List<DocumentItem> DocumentItems { get; set; } = new List<DocumentItem>();
+  }
+
+  public enum IndexType
+  {
+    String,
+    Integer,
+    Decimal,
+    Date
+  }
+
+  public class DocumentIndex
+  {
+    public string Id { get; set; } = "0";
+    public string Title { get; set; } = string.Empty;
+    public int Order { get; set; } = 0;
+    public string SearchKey { get; set; }
+
+    public IndexType Type { get; set; } = IndexType.String;
+  }
+
+  public class DocumentItem
+  {
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Type { get; set; }
+    public bool IsFolder { get; set; } = false;
+    public bool IsRoot { get; set; } = false;
+
+    public List<DocumentIndex> Indexs { get; set; } = new List<DocumentIndex>();
+
+  }
+}
