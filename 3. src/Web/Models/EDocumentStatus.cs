a85ccb09a0697d0b723c8e50ac5ef18914316d94
Hiếu Lê Văn <1012134@student.hcmus.edu.vn>
2018-07-29 21:09:59 +0700

    move code DocumentStatus to partial

---
+++ 3. src/Web/Models/EDocumentStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public enum EDocumentStatus
+    {
+        New,
+        Updated,
+        Complete
+    }
+}
---
+++ 3. src/Web/ViewModels/Elements/DocumentStatusSelectModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.ViewModels.Elements
+{
+    public class DocumentStatusSelectModel
+    {
+        public string ElementId { get; set; }
+        public string SelectedValue { get; set; }
+        public string ElementName { get; set; }
+        public string ElementClass { get; set; }
+
+    }
+}
