4f7240c9623375a80fa66641f1cbc6592070512e
Hiếu Lê Văn <1012134@student.hcmus.edu.vn>
2018-07-29 23:47:01 +0700

    update view

---
+++ 3. src/Web/ViewModels/Elements/DocumentItemListModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.ViewModels.Home;
+
+namespace Web.ViewModels.Elements
+{
+    public class DocumentItemListModel
+    {
+        public IEnumerable<DocumentIndex> TableTitles { get; set; } = new List<DocumentIndex>();
+        public IEnumerable<DocumentItem> DocumentItems { get; set; } = new List<DocumentItem>();
+
+        public string CabinetId { get; set; } = string.Empty;
+    }
+}
