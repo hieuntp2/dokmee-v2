3b780e3fe4c62ccd3e1c7de6cc6e91fecebaa3fc
Hiếu Lê Văn <1012134@student.hcmus.edu.vn>
2018-08-17 21:31:28 +0700

    update userservice

--- 3. src/Services/TempDbService/TempDbService.cs
+++ 3. src/Services/TempDbService/TempDbService.cs
@@ -2,79 +2,89 @@
 using Repositories;
 using Services.AuthService.Models;
 using System;
+using System.Data.Entity;
 using System.Linq;
 using Services.TempDbService.Exceptions;
 using Services.UserSerivce.Exceptions;
 
 namespace Services.TempDbService
 {
-  public class TempDbService : ITempDbService
-  {
-    // private 
-    private DokmeeTempEntities _dbContext;
-
-    public TempDbService()
+    public class TempDbService : ITempDbService
     {
-      _dbContext = new DokmeeTempEntities();
+        // private 
+        private DokmeeTempEntities _dbContext;
 
-    }
-
-
-    public void SetUser(string username, string password, ConnectorType type)
-    {
-      UserLogin user = GetUserLogin(username);
-      if (user == null)
-      {
-        user = new UserLogin()
+        public TempDbService()
         {
-          Username = username,
-          Password = password,
-          Created = DateTime.Now,
-          Updated = DateTime.Now,
-          Type = (int)type
-        };
+            _dbContext = new DokmeeTempEntities();
 
-        _dbContext.UserLogins.Add(user);
-      }
-      else
-      {
-        user.Password = password;
-        user.Updated = DateTime.Now; ;
-        user.Type = (int)type;
-      }
+        }
 
-      _dbContext.SaveChanges();
+
+        public void SetUser(string username, string password, ConnectorType type)
+        {
+            UserLogin user = GetUserLogin(username);
+            if (user == null)
+            {
+                user = new UserLogin()
+                {
+                    Username = username,
+                    Password = password,
+                    Created = DateTime.Now,
+                    Updated = DateTime.Now,
+                    Type = (int)type
+                };
+
+                _dbContext.UserLogins.Add(user);
+            }
+            else
+            {
+                user.Password = password;
+                user.Updated = DateTime.Now; ;
+                user.Type = (int)type;
+            }
+
+            _dbContext.SaveChanges();
+        }
+
+        public UserLogin GetUserLogin(string username)
+        {
+            var result = _dbContext.UserLogins.SingleOrDefault(t => t.Username == username);
+
+            if (result == null) return null;
+            // validate login user is timeout
+            if (result.Updated != null)
+            {
+                if (result.Updated.AddMinutes(15) < DateTime.Now)
+                {
+                    return null;
+                }
+            }
+
+            result.Updated = DateTime.Now;
+            _dbContext.Entry(result).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+            return result;
+        }
+
+        public void RemoveUserLogin(string username)
+        {
+            var userLogin = _dbContext.UserLogins.SingleOrDefault(t => t.Username == username);
+            if (userLogin != null)
+            {
+                _dbContext.UserLogins.Remove(userLogin);
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public void UpdateCabinet(string username, string cabinetId)
+        {
+            UserLogin userLogin = GetUserLogin(username);
+            if (userLogin != null)
+            {
+                userLogin.CurrentCabinetId = cabinetId;
+                _dbContext.SaveChanges();
+            }
+        }
     }
-
-    public UserLogin GetUserLogin(string username)
-    {
-      var result = _dbContext.UserLogins.SingleOrDefault(t => t.Username == username);
-
-      //if (result == null)
-      //{
-      //  throw new UserNotFoundInTempDbException("User should login first. No user infor is store");
-      //}
-      return result;
-    }
-
-    public void RemoveUserLogin(string username)
-    {
-      var userLogin = _dbContext.UserLogins.SingleOrDefault(t => t.Username == username);
-      if (userLogin != null)
-      {
-        _dbContext.UserLogins.Remove(userLogin);
-        _dbContext.SaveChanges();
-      }
-    }
-
-    public void UpdateCabinet(string username, string cabinetId)
-    {
-      UserLogin userLogin = GetUserLogin(username);
-      if (userLogin != null)
-      {
-        userLogin.CurrentCabinetId = cabinetId;
-        _dbContext.SaveChanges();
-      }
-    }
-  }
 }
